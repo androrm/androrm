f877dd86c0722f7b5cd0e911f9c4610ae2e91b7f
Philipp Giese <giese.philipp+git@gmail.com>
2011-01-14 13:52:20 +0200

    fixed a bug in DateField which resulted in loss of information after save

--- src/src/com/orm/androrm/DateField.java
+++ src/src/com/orm/androrm/DateField.java
@@ -92,19 +92,24 @@ public class DateField extends DataField<Date> {
 			cal.setTime(mValue);
 			
 			int year = cal.get(Calendar.YEAR);
-			int month = cal.get(Calendar.MONTH);
+			int month = cal.get(Calendar.MONTH) + 1;
 			int day = cal.get(Calendar.DAY_OF_MONTH);
 			int hour = cal.get(Calendar.HOUR_OF_DAY);
 			int minute = cal.get(Calendar.MINUTE);
 			int second = cal.get(Calendar.SECOND);
 			
 			String dayString = String.valueOf(day);
+			String monthString = String.valueOf(month);
 			
 			if(day < 10) {
 				dayString = "0" + dayString;
 			}
 			
-			return year + "-" + (month + 1) + "-" + dayString
+			if(month < 10) {
+				monthString = "0" + monthString;
+			}
+			
+			return year + "-" + monthString + "-" + dayString
 			 	+ "T"
 			 	+ hour + ":" + minute + ":" + second;
 		}
--- src/src/com/orm/androrm/impl/BlankModel.java
+++ src/src/com/orm/androrm/impl/BlankModel.java
@@ -1,8 +1,11 @@
 package com.orm.androrm.impl;
 
+import java.util.Date;
+
 import android.location.Location;
 
 import com.orm.androrm.CharField;
+import com.orm.androrm.DateField;
 import com.orm.androrm.LocationField;
 import com.orm.androrm.Model;
 
@@ -10,12 +13,14 @@ public class BlankModel extends Model {
 
 	protected CharField mName;
 	protected LocationField mLocation;
+	protected DateField mDate;
 	
 	public BlankModel() {
 		super();
 		
 		mName = new CharField();
 		mLocation = new LocationField();
+		mDate = new DateField();
 	}
 	
 	public void setName(String name) {
@@ -32,5 +37,13 @@ public class BlankModel extends Model {
 	
 	public Location getLocation() {
 		return mLocation.get();
+	}
+	
+	public Date getDate() {
+		return mDate.get();
+	}
+	
+	public void setDate(Date date) {
+		mDate.set(date);
 	}
 }
