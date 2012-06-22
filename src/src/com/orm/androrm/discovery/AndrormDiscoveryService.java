4bfc236a6f6445f7c2db1c14ffec8f098e8cd044
Philipp Giese <giese.philipp+git@gmail.com>
2012-06-22 09:48:52 +0200

    stubbed tests and classes for model discovery

---
+++ src/src/com/orm/androrm/discovery/AndrormDiscoveryService.java
@@ -0,0 +1,16 @@
+package com.orm.androrm.discovery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.orm.androrm.Model;
+
+public class AndrormDiscoveryService {
+
+	public List<Class<? extends Model>> discover(String pkg) {
+		List<Class<? extends Model>> models = new ArrayList<Class<? extends Model>>();
+		
+		return models;
+	}
+	
+}
---
+++ test/src/com/orm/androrm/test/discovery/AllTest.java
@@ -0,0 +1,13 @@
+package com.orm.androrm.test.discovery;
+
+import junit.framework.TestSuite;
+
+public class AllTest extends TestSuite {
+
+	public static TestSuite suite() {
+		TestSuite suite = new TestSuite();
+		
+		return suite;
+	}
+	
+}
